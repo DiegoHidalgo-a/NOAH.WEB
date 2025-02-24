bd4a21a2288954cbad8f1f7a7b4e4bda159e6929
Diego Hidalgo <diegoarmandohidalgo3@gmail.com>
2025-02-24 16:54:47 -0600

    cambios 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,19 @@ export default {
   },
   plugins: [],
 };
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      backgroundSize: {
+        '200': '200% auto',
+      },
+      backgroundPosition: {
+        'pos-100': '100% center',
+      }
+    }
+  },
+  plugins: [
+    require('tailwindcss-animated')
+  ]
+}
